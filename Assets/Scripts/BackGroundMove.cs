c80cfa8543c6504ed2046905dae43c86efea604b
thanhcongvq347 <gentlemenitluxury@gmail.com>
2021-09-21 21:29:50 +0700

    thu nghiem voi unity 4(5)

---
+++ Assets/Scripts/BackGroundMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMove : MonoBehaviour {
+    public float moveSpeed;
+    public float moveRange;
+	// Use this for initialization
+    private Vector3 oldPosition;
+    private GameObject obj;
+	void Start () {
+        obj = gameObject;
+        oldPosition = obj.transform.position;
+        moveSpeed = 5;
+        moveRange = 20;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(-1 * Time.deltaTime*moveSpeed, transform.position.y, 0));
+        if (Vector3.Distance(oldPosition, obj.transform.position) > moveRange)
+        {
+            obj.transform.position = oldPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+    public float flyPower;
+    GameObject obj;
+	// Use this for initialization
+	void Start () {
+        obj = gameObject;
+        flyPower = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            obj.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, flyPower));
+
+        }
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EndGame();
+    }
+    void EndGame()
+    {
+        Debug.Log("EndGame");
+    }
+}
